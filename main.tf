27f9169889c5a4a07f0b073c176281086e11494f
Oscar Ruiz <krono@live.com.mx>
2026-04-28 23:08:40 -0600

    Troubleshot subnet references

--- main.tf
+++ main.tf
@@ -18,12 +18,14 @@ module "vpc_module" {
 # ECS Module
 module "ecs_module" {
   source  = "app.terraform.io/oruiz/ecs_module/aws"
-  version = "1.0.1"
+  version = "1.0.2"
 
   allowed_ip   = var.allowed_ip
   key_name     = var.key_name
   docker_image = var.docker_image
   vpc_id       = module.vpc_module.vpc_id
+  private_app_subnet_id = module.vpc_module.private_app_subnet_id
+  public_subnet_id      = module.vpc_module.public_subnet_id
 
   depends_on = [module.vpc_module]
 }
