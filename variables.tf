87ab2b114a25a053bf578f8e3270c06592845942
Oscar Ruiz <39598776+krhno@users.noreply.github.com>
2026-05-06 17:39:13 -0600

    Add public subnet CIDR blocks for AZ1 and AZ2
    Added CIDR blocks for public subnets in two availability zones and updated the default value for the private app subnet.

--- variables.tf
+++ variables.tf
@@ -25,16 +25,22 @@ variable "vpc_cidr" {
   default     = "192.168.0.0/16"
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_cidr_az1" {
   type        = string
   description = "Public subnet CIDR block"
   default     = "192.168.0.0/24"
 }
 
+variable "public_subnet_cidr_az2" {
+  type        = string
+  description = "Public subnet CIDR block"
+  default     = "192.168.1.0/24"
+}
+
 variable "private_app_subnet_cidr" {
   type        = string
   description = "Private app subnet CIDR block"
-  default     = "192.168.1.0/24"
+  default     = "192.168.2.0/24"
 }
 
 # ECS Variables
